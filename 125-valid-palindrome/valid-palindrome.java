da332f199c53dd31170e1d9d5c089be5ce595419
Sai Sindu <135440436+saisindusrig@users.noreply.github.com>
2024-07-15 12:58:36 +0530

    Time: 4 ms (54.74%) | Memory: 43.1 MB (59.92%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,23 +1,21 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        if(s == null || s.length()==0) {
-			return true;
+      if (s == null || s.length()==0){
+        return true;
+      }
+      String r= s.toLowerCase();
+      StringBuilder sb = new StringBuilder();
+      for(int i=0;i<r.length();i++){
+        char c= r.charAt(i);
+        if(Character.isLetterOrDigit(c)){
+            sb.append(c);
         }
-        String r =s.toLowerCase();
-        StringBuilder sb = new StringBuilder();
-        for(int i=0;i<r.length();i++){
-            char c = r.charAt(i);
-            if(Character.isLetterOrDigit(c)){ //digits are also allowed
-                sb.append(c);
-            }
+      }
+      for(int i=0;i<sb.length()/2;i++){
+        if(sb.charAt(i)!=sb.charAt(sb.length()-1-i)){
+            return false;
         }
-        for(int i=0;i<sb.length()/2;i++) {
-			char start = sb.charAt(i);
-			char end = sb.charAt(sb.length()-1-i);
-			if(start!=end) {
-				return false;
-			}
-		}
-		return true;
+      }
+      return true;
     }
 }
