c6324520003a9cce9c64503fe41949a36834ff15
Sai Sindu <135440436+saisindusrig@users.noreply.github.com>
2024-07-17 11:56:14 +0530

    Time: 1 ms (81.36%) | Memory: 41.4 MB (35.08%) - LeetSync

--- 961-long-pressed-name/long-pressed-name.java
+++ 961-long-pressed-name/long-pressed-name.java
@@ -1,25 +1,25 @@
 class Solution {
     public boolean isLongPressedName(String name, String typed) {
-        int i=0; //name
-        int j=0; //typed
-        int n= name.length();
-        int m= typed.length();
-        while(i<n){ //for each c in name
-            int c1 =0; 
+        int i=0;
+        int j=0;
+        int n = name.length();
+        int t = typed.length();
+        while(i<n){
+            int c1=0;
             int c2=0;
-            char c = name.charAt(i); //take a char(c) in name
-            while(i<n && name.charAt(i)==c){  
-                i++;
-                c1++; //check the times c is repeating in name
-            }while(j<m && typed.charAt(j)==c){
+            char c = name.charAt(i);
+            while(i<n && name.charAt(i)==c){
+                i++; //increase in position of i in name
+                c1++;
+            }while (j<t && typed.charAt(j)==c){
                 j++;
-                c2++; //check the times c is repeating in typed
+                c2++;
             }
-            if(c1>c2){ //if typed doesn't have the count of original name chars
-                return false; //then false
+            if(c1>c2){
+                return false;
             }
         }
-        return j<m? false : true; //if extra characters are left, then it is false as it is not longpressed.
+        return j<t? false: true;
     }
     
 }
