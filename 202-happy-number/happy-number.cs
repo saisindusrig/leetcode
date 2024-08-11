16b8b608c6d5ce77eb80dce7f0573e42422824a9
Sai Sindu <135440436+saisindusrig@users.noreply.github.com>
2024-08-11 15:54:01 +0530

    Time: 29 ms (72.56%) | Memory: 29.5 MB (39.76%) - LeetSync

--- 202-happy-number/happy-number.cs
+++ 202-happy-number/happy-number.cs
@@ -1,19 +1,19 @@
 public class Solution {
     public bool IsHappy(int n) {
-        int fast = n;
-        int slow =n;
-        do{
-            slow = findSquare(slow);
-            fast = findSquare(findSquare(fast));
-        }while(slow!=fast);
-        return slow==1;
+        var seen = new HashSet<int>();
+        while (n != 1 && !seen.Contains(n)) {
+            seen.Add(n);
+            n = FindSquare(n);
+        }
+        return n == 1;
     }
-    public int findSquare(int number){
-        int sum= 0;
-        while(number>0){
-            int rem = number %10;
-            sum+=rem*rem;
-            number = number/10;
+
+    private int FindSquare(int number) {
+        int sum = 0;
+        while (number > 0) {
+            int rem = number % 10;
+            sum += rem * rem;
+            number /= 10;
         }
         return sum;
     }
