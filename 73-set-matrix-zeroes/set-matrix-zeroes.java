8bf24373881df69ba4fd26ee6fa1fa966d3dd78c
Sai Sindu Sri G <135440436+saisindusrig@users.noreply.github.com>
2024-09-19 22:05:40 +0530

    Time: 0 ms (100.00%) | Memory: 45.7 MB (18.29%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -4,18 +4,18 @@ class Solution {
         int n = matrix[0].length;
         boolean[] rows = new boolean[m];
         boolean[] col = new boolean[n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(matrix[i][j]==0){
-                    rows[i]=true;
-                    col[j]=true;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    rows[i] = true;
+                    col[j] = true;
                 }
             }
         }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(rows[i] || col[j]){
-                    matrix[i][j]=0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (rows[i] || col[j]) {
+                    matrix[i][j] = 0;
                 }
             }
         }
