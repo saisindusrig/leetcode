f9ef12a8ffecd981649486a42de6aa807910bc9e
Sai Sindu Sri G <135440436+saisindusrig@users.noreply.github.com>
2025-03-27 22:57:11 +0530

    Time: 17 ms (66.03%) | Memory: 56.2 MB (90.38%) - LeetSync

---
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.java
@@ -0,0 +1,57 @@
+class Solution {
+    public int minimumIndex(List<Integer> nums) {
+        
+        if(nums.size()==1) return -1;
+        
+        int dom = majority(nums);
+       
+        int tc= 0;
+        for (int num : nums) {
+            if (num == dom) {
+                tc++;
+            }
+        }
+        int l=0;
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums.get(i)==dom){
+                l++;
+            }
+        
+            int r = tc-l;
+            int ls = i+1;
+            int rs = nums.size()-ls;
+            if(2*l>ls && 2*r>rs){
+                return i;
+            }
+}
+        
+        return -1;
+    
+    }
+   
+    public int majority(List<Integer> a){
+        int v=0,c=-1;
+        for(int i=0;i<a.size();i++){
+            if(v==0){
+                c= a.get(i);
+                v=1;
+            }
+            else{
+                if(a.get(i)==c){
+                    v++;
+                }else{
+                    v--;
+                }
+            }
+        }
+     
+        v=0;
+        for(int i=0;i<a.size();i++){
+            if(a.get(i)==c){
+                v++;
+            }
+        }
+        
+        return (v > a.size() / 2) ? c : -1;
+    }
+}
